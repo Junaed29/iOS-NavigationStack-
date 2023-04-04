84ab1241f990a6a05265dd46b8b24b3a7f8bd205
Junaed <junaed1995@gmail.com>
2023-04-04 11:49:19 +0600

    Initial Commit

---
+++ iOS-NavigationStack/iOS_NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_NavigationStackApp.swift
+//  iOS-NavigationStack
+//
+//  Created by Junaed Muhammad Chowdhury on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
