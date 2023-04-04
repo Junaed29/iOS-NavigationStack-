4364a10b0e8013a0569a4eb9a0ec867488abaea2
Junaed <junaed1995@gmail.com>
2023-04-04 12:15:36 +0600

    * Navigation Stack added

--- iOS-NavigationStack/ContentView.swift
+++ iOS-NavigationStack/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var platform: [Platform] = [
+        .init(name: "Xbox", imageName: "xbox.logo", color: .green),
+        .init(name: "Playstation", imageName: "playstation.logo", color: .indigo),
+        .init(name: "PC", imageName: "pc", color: .pink),
+        .init(name: "Mobile", imageName: "iphone", color: .mint)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section("Platforms") {
+                    ForEach(platform, id: \.name) { platform in
+                        Label(platform.name, systemImage: platform.imageName)
+                            .foregroundColor(platform.color)
+                    }
+                }
+            }
+            .navigationTitle("Gaming")
         }
-        .padding()
     }
 }
 
@@ -23,4 +35,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct Platform{
+    let name: String
+    let imageName: String
+    let color: Color
 }
